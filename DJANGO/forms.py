96b92ea14ee32f84f5ebe1ee7b4e96f80409f00a
Martin Kagema <martinkagema01@gmail.com>
2026-03-31 16:13:13 +0300

    Additional commits

---
+++ DJANGO/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class PDFUploadForm(forms.Form):
+    pdf_file = forms.FileField(
+        label = 'Upload PDF', required=True
+    )
--- DJANGO/views.py
+++ DJANGO/views.py
@@ -1,4 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PDFUploadForm
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from langchain_community.vectorstores import Qdrant
+from qdrant_client import QdrantClient
+from langchain.chains import retrieval_qa
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
+from langchain_groq import ChatGroq
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.getenv('GROQ_API_KEY')
+llm = ChatGroq(temperature=0.6,model='llama-3.1-8b-instant',api_key=apikey)
+vector_search = None
+
+
+
+
+
 
 # Create your views here.
 def home(requests):
